5664f3b7cb11d1fda781d9d029984294633c2a2e
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-06 00:20:35 +0000

    setting GET route for notes to display test data

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,11 +1,19 @@
-const noteData = require("db/db.json");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+
+let noteData = [];
 
 module.exports = (app) => {
   app.get("/api/notes", (req, res) => {
-    res.json(noteData);
+    fs.readFile("db/db.json", (err, data) => {
+      if (err) throw err;
+      let noteJSON = JSON.parse(data);
+      for (const note in noteJSON) {
+        noteData.push(noteJSON[note]);
+      }
+      res.json(noteData);
+    });
   });
 
-  app.post("/api/notes", (req, res) => {
-    noteData.push(req.body);
-  });
+  app.post("/api/notes", (req, res) => {});
 };
