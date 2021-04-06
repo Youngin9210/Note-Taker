e08b55d4b65fc610889f085fabfc6b01e57b1140
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-06 18:19:59 +0000

    writing data to db.json with POST route

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,21 +1,29 @@
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 
-let noteData = [];
-
 module.exports = (app) => {
   app.get("/api/notes", (req, res) => {
     fs.readFile("db/db.json", (err, data) => {
       if (err) throw err;
-      let noteJSON = JSON.parse(data);
-      for (const note in noteJSON) {
-        noteData.push(noteJSON[note]);
-        noteJSON[note].id = uuidv4();
-      }
-      console.log(noteData);
-      res.json(noteData);
+      let notes = JSON.parse(data);
+      res.json(notes);
     });
   });
 
-  app.post("/api/notes", (req, res) => {});
+  app.post("/api/notes", (req, res) => {
+    fs.readFile("db/db.json", (err, data) => {
+      if (err) throw err;
+      let notes = JSON.parse(data);
+      let newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4(),
+      };
+      notes.push(newNote);
+
+      fs.writeFile("db/db.json", JSON.stringify(notes, null, 2), (err) => {
+        if (err) throw err;
+      });
+    });
+  });
 };
