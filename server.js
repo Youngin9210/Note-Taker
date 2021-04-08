fe4191caa6a254118c9df704435318a107fe5176
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-08 16:53:48 +0000

    working on heroku error “cannot find module ‘./routes/apiRoutes’. Changed ./route to ./Route

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const app = express();
 
-const path = require("path");
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./Routes/apiRoutes.js")(app);
+require("./Routes/htmlRoutes.js")(app);
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
