b8abc0036f18d3de09b4e56ef0d79825e4cf7faa
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-08 02:16:43 +0000

    still trouble shooting for Heroku H10 err

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-require("./routes/htmlRoutes")(app);
 require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
